ff76f0fd61f2f8487f2826f34e3c6e1024a0fc07
Robert Means <robert@robertmeans.com>
2023-04-17 11:23:01 -0600

    updates to desktop

--- gruntfile.js
+++ gruntfile.js
@@ -30,7 +30,11 @@ module.exports = function(grunt) {
 		  	files: {
 		  		// 'style.css': 'components/sass/style.scss' // default is Darkmode
 		  		// 'style-spring.css': 'components/sass/style.scss'
-		  		'style-classic.css': 'components/sass/style.scss'
+		  		// 'style-classic.css': 'components/sass/style.scss'
+
+          'style.css': 'components/sass/style.scss', // default is Darkmode
+          'style-spring.css': 'components/sass/style-spring.scss',
+          'style-classic.css': 'components/sass/style-classic.scss'
 		  	}
 		  }
 		},
