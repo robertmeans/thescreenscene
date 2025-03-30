e61af12aa6ef993007550066b149b01ea84be1d4
Robert Means <robert@robertmeans.com>
2025-03-30 10:42:40 -0600

    added ai logos

--- _includes/search_ai.php
+++ _includes/search_ai.php
@@ -1,7 +1,7 @@
 <div id="ai" class="searches">
-<div class="perai ch"><a class="<?php if (isset($s) && $s[0] == "3") { echo 'cgpt'; } ?>" href="https://chatgpt.com/" target="_blank">ChatGPT</a></div>
-<div class="perai ge"><a href="https://gemini.google.com/" target="_blank">Gemini</a></div>
-<div class="perai gr"><a href="https://grok.com/" target="_blank">Grok</a></div>
-<div class="perai pe"><a class="<?php if (isset($s) && $s[0] == "3") { echo 'perpr'; } ?>" href="https://perplexity.ai/" target="_blank">Perplexity</a></div>
+  <div class="perai ch"><a class="<?php if (isset($s) && $s[0] == "3") { echo 'cgpt'; } ?>" href="https://chatgpt.com/" target="_blank"><img src="_images/chatgpt-logo-white.webp"><span class="mg">ChatGPT</span></a></div>
+  <div class="perai ge"><a href="https://gemini.google.com/" target="_blank"><img src="_images/gemini-logo-color.webp"><span class="mg">Gemini</span></a></div>
+  <div class="perai gr"><a href="https://grok.com/" target="_blank"><img src="_images/grok-logo-white.webp"><span class="mg">Grok</span></a></div>
+  <div class="perai pe"><a class="<?php if (isset($s) && $s[0] == "3") { echo 'perpr'; } ?>" href="https://perplexity.ai/" target="_blank"><img src="_images/perplexity-logo-white.webp"><span class="mg">Perplexity</span></a></div>
 
 </div><!-- end AI -->
--- _includes_static/static_ai.php
+++ _includes_static/static_ai.php
@@ -1,7 +1,7 @@
 <div id="ai" class="searches">
-<div class="perai ch"><a class="<?php if ($r[0] == "3") { echo 'cgpt'; } ?>" target="_blank">ChatGPT</a></div>
-<div class="perai ge"><a href="https://gemini.google.com/" target="_blank">Gemini</a></div>
-<div class="perai gr"><a href="https://grok.com/" target="_blank">Grok</a></div>
-<div class="perai pe"><a class="<?php if ($r[0] == "3") { echo 'perpr'; } ?>" target="_blank">Perplexity</a></div>
+<div class="perai ch"><a class="<?php if ($r[0] == "3") { echo 'cgpt'; } ?>" target="_blank"><span class="mg">ChatGPT</span></a></div>
+<div class="perai ge"><a href="https://gemini.google.com/" target="_blank"><span class="mg">Gemini</span></a></div>
+<div class="perai gr"><a href="https://grok.com/" target="_blank"><span class="mg">Grok</span></a></div>
+<div class="perai pe"><a class="<?php if ($r[0] == "3") { echo 'perpr'; } ?>" target="_blank"><span class="mg">Perplexity</span></a></div>
 
 </div><!-- end AI -->
